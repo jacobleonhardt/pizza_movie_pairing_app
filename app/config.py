0413ce7c625e957e10a37885f82ec69685fe2e6a
leonhardt <jacob.leonhardt649@gmail.com>
2021-06-29 12:16:18 -0500

    heroku setup

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
   SECRET_KEY=os.environ.get('SECRET_KEY')
+  TMDb_API=os.environ.get('TMDb_API')
   SQLALCHEMY_TRACK_MODIFICATIONS=False
   SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
   SQLALCHEMY_ECHO=True
