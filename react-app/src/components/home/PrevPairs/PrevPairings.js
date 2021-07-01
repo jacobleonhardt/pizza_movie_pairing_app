953c00cbb7c07dfe6e04a14b35da91902b7ac9e7
leonhardt <jacob.leonhardt649@gmail.com>
2021-07-01 14:42:43 -0500

    previous pairings styling

--- react-app/src/components/home/PrevPairs/PrevPairings.js
+++ react-app/src/components/home/PrevPairs/PrevPairings.js
@@ -5,17 +5,19 @@ const PrevPairingCard = (movie) => {
     return (
         <div className="previous-pairing">
             <div id="pairing-display" className="solid-block">
-                <div className="left">
+                <div className="first">
                     <img src={`https://image.tmdb.org/t/p/w500${movie.movie.poster}`} />
                 </div>
-                <div className="middle">
+                <div className="second">
                     <h4>{movie.movie.title} ({movie.movie.release_date ? movie.movie.release_date.slice(0,4) : 'Unknown'})</h4>
+                </div>
+                <div className="three">
                     <h4>{movie.movie.pizza}</h4>
                 </div>
-                <div className="right">
-                    <ion-icon name="thumbs-up-outline"></ion-icon>
-                    <ion-icon name="thumbs-down-outline"></ion-icon>
-                    <ion-icon name="close-circle-outline"></ion-icon>
+                <div className="four">
+                    <button><ion-icon name="thumbs-up-outline"></ion-icon></button>
+                    <button><ion-icon name="thumbs-down-outline"></ion-icon></button>
+                    <button><ion-icon name="trash-outline"></ion-icon></button>
                 </div>
             </div>
         </div>
