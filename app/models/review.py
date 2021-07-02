1580028f616db30226f8fc51eb834fc4ed35ffd3
leonhardt <jacob.leonhardt649@gmail.com>
2021-07-01 21:24:01 -0500

    working on upload to heroku

--- app/models/review.py
+++ app/models/review.py
@@ -1,4 +1,5 @@
 from .db import db
+from datetime import datetime
 
 class Review(db.Model):
     __tablename__ = 'reviews'
@@ -7,6 +8,7 @@ class Review(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     pairing_id = db.Column(db.Integer, db.ForeignKey('pairings.id'))
     good = db.Column(db.Boolean)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     user = db.relationship("User", back_populates="review")
     pair = db.relationship("Pairing")
@@ -16,5 +18,6 @@ class Review(db.Model):
             "id": self.id,
             "user_id": self.user_id,
             "pairing_uid": self.pairing_id,
-            "good": self.good
+            "good": self.good,
+            "created_at": self.created_at
         }
