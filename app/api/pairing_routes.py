9ee52886c319b982a17b25dc87d7e04de103ff41
leonhardt <jacob.leonhardt649@gmail.com>
2021-07-01 22:08:30 -0500

    order of previous pairs

--- app/api/pairing_routes.py
+++ app/api/pairing_routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import db, Pairing
-from sqlalchemy import desc
 import requests
 import random
 
@@ -11,7 +10,7 @@ pairing_routes = Blueprint('new', __name__)
 @pairing_routes.route('/<int:userId>')
 @login_required
 def getPrePairings(userId):
-    prev = Pairing.query.filter(Pairing.user_id == userId).all()
+    prev = Pairing.query.filter(Pairing.user_id == userId).order_by(Pairing.created_at.desc()).all()
     prev_list = [movie.to_dict() for movie in prev]
     return jsonify(prev_list)
 
@@ -21,7 +20,7 @@ def pairing(userId):
     req = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=d4b83eae239cd5168bcdc521eeea13b6&include_adult=false&language=en-US&certification=PG-13&with_genres=28")
     response = req.json();
     results = response["results"];
-    prev = Pairing.query.filter(Pairing.user_id == userId).order_by(desc(created_at)).all()
+    prev = Pairing.query.filter(Pairing.user_id == userId).all()
     possible_selections = []
 
     for movie in results:
