21531491a04ea64f6e18e2e914b6b597e9102eee
leonhardt <jacob.leonhardt649@gmail.com>
2021-07-05 21:08:07 -0500

    trying route specifically for favicon

--- app/__init__.py
+++ app/__init__.py
@@ -70,9 +70,13 @@ def inject_csrf_token(response):
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-@app.route('/favicon.ico')
 def react_root(path):
     print("path", path)
     if path == 'favicon.ico':
         return app.send_static_file('favicon.ico')
     return app.send_static_file('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static', 'images'),
+        'favicon.ico', mimetype='image/png')
