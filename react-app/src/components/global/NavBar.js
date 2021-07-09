91fc46fa4ab0a02fd763c2e324060d54e244b249
leonhardt <jacob.leonhardt649@gmail.com>
2021-07-09 18:56:18 -0500

    add demo user login in nav

--- react-app/src/components/global/NavBar.js
+++ react-app/src/components/global/NavBar.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useHistory } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
+import { loginDemo } from '../../store/session';
 import './nav_footer.css';
 
 const NavBar = () => {
-
+  const dispatch = useDispatch()
   const user = useSelector(state => state.session.user)
+  const history = useHistory()
+
+  const demo = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(loginDemo());
+    history.push(`/`)
+    }
 
   return (
     <nav>
@@ -18,6 +26,9 @@ const NavBar = () => {
       <>
           <NavLink to="/login" exact={true} activeClassName="active">
             Login
+          </NavLink>
+          <NavLink to="#" onClick={demo} exact={true} className="nav-button" activeClassName="active">
+            Demo User
           </NavLink>
           <NavLink to="/new-account" exact={true} className="nav-button" activeClassName="active">
             New Account
